a35e27d363320367095e940bd654ad69052268c8
Anthony Lu <anthony.x.lu@pwc.com>
2019-03-27 15:10:35 +0800

    got all products first

--- app.js
+++ app.js
@@ -105,173 +105,172 @@ new CronJob('00 */2 * * * *', function () {
           all_cats = JSON.parse(results[1])
           all_domains = JSON.parse(results[2])
           all_urls = JSON.parse(results[3])
-          MongoClient.connect(mongodb_url, function (err, db) {
-            if (err) throw err;
-            var dbo = db.db(db_name);
-            async function getDescriptions(total_promote_cat) {
-              for (let cat of total_promote_cat) {
-                const response = await dbo.collection("description").aggregate([{
-                  $match: {
-                    category: cat.name
-                  }
-                }, {
-                  $sample: {
-                    size: all_domains.length*5
-                  }
-                }])
-                  .toArray()
-                cat.descriptions = response
 
-              }
-              return total_promote_cat
-            }
-            const total_promote_cat_with_desc = getDescriptions(all_cats)
-            total_promote_cat_with_desc.then((res) => {
-              all_cats = res
-              if (all_domains.length > 0) {
-                for (let index = 0; index < all_domains.length; index++) {
-                  const this_domain = all_domains[index];
-                  let post_title;
-                  if (this_domain.auto_post && this_domain.login_url && this_domain.login_username && this_domain.login_password && this_domain.auto_post) {
-                    let published_keyword = this_domain.published_keyword
-                    let un_published_keyword = _.differenceBy(all_keywords, published_keyword, "id")
-                    if (un_published_keyword.length > 0) {
-                      // arrange data for post
-                      let selected_keyword_obj = _.sample(un_published_keyword)
-                      post_title = selected_keyword_obj.name
-                      published_keyword.push(selected_keyword_obj)
-                      let link_content = getLinkStr.links(this_domain.login_url, all_cats, all_urls,post_title);
-                      link_content = `<strong>${post_title}</strong> ${link_content}`;
-                      console.log(`to post for (${this_domain.login_url})`)
-                      let post = {}
-                      post.title = post_title;
-                      post.content = link_content
-                      post.status = "publish"
-                      let subtract_minutes = _.random(0, 120)
-                      post.date = moment().subtract(subtract_minutes, 'minutes').format();
-                      let post_category
-                      if (this_domain.postNewsCats && this_domain.postNewsCats.length > 0) {
-                        post_category = [_.sample(this_domain.postNewsCats).catName]
-                      } else {
-                        post_category = ["plastic crates"]
+          rp(products_auth_options)
+            .then(function (response) {
+                  console.log(`Got jwt==================${response.jwt}`)
+                  return response.jwt
+            })
+            .then(function (p_jwt) {
+              // got random product
+              return rp({
+                  method: 'GET',
+                  uri: `${product_api_domin}${product_api_url}&_limit=${product_limit}`,
+                  headers: {
+                      Authorization: `Bearer ${p_jwt}`
+                  },
+                  json: true
+                })
+                .catch(function (err) {
+                  console.log(err.message)
+                })
+            .then(function(products){
+              console.log(`got products list`)
+              let all_products = products
+              MongoClient.connect(mongodb_url, function (err, db) {
+                if (err) throw err;
+                var dbo = db.db(db_name);
+                async function getDescriptions(total_promote_cat) {
+                  for (let cat of total_promote_cat) {
+                    const response = await dbo.collection("description").aggregate([{
+                      $match: {
+                        category: cat.name
                       }
-                      post.termNames = {
-                        "category": post_category,
+                    }, {
+                      $sample: {
+                        size: all_domains.length*5
                       }
+                    }])
+                      .toArray()
+                    cat.descriptions = response
 
-                      // arrange data for image
-                      let category_id = selected_keyword_obj.category._id
-                      
-                      let product_cat = getCategoryById(category_id)
-                      let product_filter = `?category=${product_cat}`
-                      var WP_client = wordpress.createClient({
-                        url: this_domain.login_url,
-                        username: this_domain.login_username,
-                        password: this_domain.login_password
-                      });
+                  }
+                  return total_promote_cat
+                }
+                const total_promote_cat_with_desc = getDescriptions(all_cats)
+                total_promote_cat_with_desc.then((res) => {
+                  all_cats = res
+                  if (all_domains.length > 0) {
+                    for (let index = 0; index < all_domains.length; index++) {
+                      const this_domain = all_domains[index];
+                      let post_title;
+                      if (this_domain.auto_post && this_domain.login_url && this_domain.login_username && this_domain.login_password && this_domain.auto_post) {
+                        let published_keyword = this_domain.published_keyword
+                        let un_published_keyword = _.differenceBy(all_keywords, published_keyword, "id")
+                        if (un_published_keyword.length > 0) {
+                          // arrange data for post
+                          let selected_keyword_obj = _.sample(un_published_keyword)
+                          post_title = selected_keyword_obj.name
+                          published_keyword.push(selected_keyword_obj)
+                          let link_content = getLinkStr.links(this_domain.login_url, all_cats, all_urls,post_title);
+                          link_content = `<strong>${post_title}</strong> ${link_content}`;
+                          console.log(`to post for (${this_domain.login_url})`)
+                          let post = {}
+                          post.title = post_title;
+                          post.content = link_content
+                          post.status = "publish"
+                          let subtract_minutes = _.random(0, 120)
+                          post.date = moment().subtract(subtract_minutes, 'minutes').format();
+                          let post_category
+                          if (this_domain.postNewsCats && this_domain.postNewsCats.length > 0) {
+                            post_category = [_.sample(this_domain.postNewsCats).catName]
+                          } else {
+                            post_category = ["plastic crates"]
+                          }
+                          post.termNames = {
+                            "category": post_category,
+                          }
 
-                      rp(products_auth_options)
-                        .then(function (response) {
-                            console.log(`Got jwt==================${response.jwt}`)
-                            return response.jwt
-                        })
-                        .then(function (jwt) {
-                          // got random product
-                          return rp({
-                              method: 'GET',
-                              uri: `${product_api_domin}${product_api_url}${product_filter}&_limit=${product_limit}`,
-                              headers: {
-                                  Authorization: `Bearer ${jwt}`
-                              },
-                              json: true
-                            })
-                            .then(function (res) {
-                              let products = res
-                              if(products.length>0){
-                                let random_product = _.sample(products)
-                                return random_product
-                              }
-                              return ``
-                            })
-                            .catch(function (err) {
-                              console.log(err.message)
-                          })
-                        })
-                        .then(function(random_product){
-                          // choose one image and get image info like width,mime type
+                          // arrange data for image
+                          let category_id = selected_keyword_obj.category._id
+                          
+                          let product_cat = getCategoryById(category_id)
+                          var WP_client = wordpress.createClient({
+                            url: this_domain.login_url,
+                            username: this_domain.login_username,
+                            password: this_domain.login_password
+                          });
+
+                          let random_product = _.sample(all_products.filter(item=>item.category === product_cat))
                           if(random_product.images && random_product.images.length>0){
                             let random_img_url = _.sample(random_product.images).url
-                            return probe(random_img_url).
+                            probe(random_img_url).
                               then(result => {
                                 console.log(`got image info like mime ${result.mime}`); // =>
                                 return result
-                            });
-                          }
-                        })
-                        .then(function(result){
-                          let data = {}
-                          data.name = `${post_title.replace(/ +/g,"-")}.${result.url.split('.').pop()}`
-                          data.type = result.mime
-                          data.width = result.width
-                          data.height = result.height
-                          data.alt = post_title
-                          let url = result.url
-                          rp({url, encoding: null })
-                          .then(function (buffer) {
-                            data.bits = buffer
-                            // post image
-                            WP_client.uploadFile( data, function(error, file){
-                              if(error){
-                                console.log(error)
-                                return
-                              }
-                              post.thumbnail = file.attachment_id
-                              console.log(file.link)
-                              // post article and bind feature image
-                              // console.log(JSON.stringify(post))
-                              WP_client.newPost( post, function(err, res){
-                                if(err){
-                                  console.log(error)
-                                  return
-                                }
-                                console.log(`new post id:${res} for website:${this_domain.login_url} --- binded with image (${file.attachment_id}) (${file.file}) time: ${new Date()}`)
-                                //update relation for this domain and keyword
-                                rp({
-                                  method: 'PUT',
-                                  uri: api_root_path + domain_path + "/" + this_domain.id,
-                                  headers: api_header_obj,
-                                  body: {
-                                    published_keyword: published_keyword
-                                  },
-                                  json: true
-                                })
-                                  .then(function (res) {
-                                    console.log(`link keyword (${selected_keyword_obj.name}) and domain (${this_domain.name}) success`)
+                              })
+                              .then(function(result){
+                                let data = {}
+                                data.name = `${post_title.replace(/ +/g,"-")}.${result.url.split('.').pop()}`
+                                data.type = result.mime
+                                data.width = result.width
+                                data.height = result.height
+                                data.alt = post_title
+                                let url = result.url
+                                rp({url, encoding: null })
+                                  .then(function (buffer) {
+                                    data.bits = buffer
+                                    // post image
+                                    WP_client.uploadFile( data, function(error, file){
+                                      if(error){
+                                        console.log(error)
+                                        return
+                                      }
+                                      post.thumbnail = file.attachment_id
+                                      console.log(`post image success for domain(${this_domain.login_url}) (${post_title})`)
+                                      // post article and bind feature image
+                                      // console.log(JSON.stringify(post))
+                                      WP_client.newPost( post, function(err, res){
+                                        if(err){
+                                          console.log(error)
+                                          return
+                                        }
+                                        console.log(`new post id:${res} for website:${this_domain.login_url} --- binded with image (${file.attachment_id}) (${file.file}) time: ${new Date()}`)
+                                        //update relation for this domain and keyword
+                                        rp({
+                                          method: 'PUT',
+                                          uri: api_root_path + domain_path + "/" + this_domain.id,
+                                          headers: api_header_obj,
+                                          body: {
+                                            published_keyword: published_keyword
+                                          },
+                                          json: true
+                                        })
+                                          .then(function (res) {
+                                            console.log(`link keyword (${selected_keyword_obj.name}) and domain (${this_domain.name}) success`)
+                                          })
+                                          .catch(function (err) {
+                                            console.log(err.message)
+                                          });
+                                      })
+                                      
+                                    })
                                   })
                                   .catch(function (err) {
-                                    console.log(err.message)
+                                    console.log(`probe: get image info err`)
                                   });
                               })
-                              
-                            })
-                          })
-                          .catch(function (err) {
-                            console.log(`probe: get image info err`)
-                          });
-                        })
-                        .catch(function (err) {
-                          console.log(err.message)
-                        })
-                    } else {
-                      console.log(`all keywords had been post for domain (${this_domain.name}),please add keywords!`)
+                              .catch(function (err) {
+                                console.log(err.message)
+                              });
+                          }
+                        } else {
+                          console.log(`all keywords had been post for domain (${this_domain.name}),please add keywords!`)
+                        }
+                      } else {
+                        console.log(`cannot login to this domain (${this_domain.name}),please setup login info!`)
+                      }
                     }
-                  } else {
-                    console.log(`cannot login to this domain (${this_domain.name}),please setup login info!`)
                   }
-                }
-              }
+                })
+              })
+            })
+            .catch(function (err) {
+              console.log(err.message)
             })
           })
+
+          
         })
         .catch(function (err) {
           console.log(err.message)
