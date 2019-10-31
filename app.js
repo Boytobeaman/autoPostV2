0a309bad9dd04d8bd25daf592db2ef948c3847f6
Your Name <you@example.com>
2019-10-31 08:13:11 +0800

    add file log

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ function getCategoryById(id){
   return category_id_mapping[id]
 }
 
-console.log(__dirname)
+console.log(`__filename: ${__filename}`)
 console.log(`app runing`)
 var CronJob = require('cron').CronJob;
 new CronJob('00 00 */12 * * *', function () {
